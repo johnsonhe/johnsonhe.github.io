7d725533e6b6b47852ff2e17fb2312521b8ad389
johnsonhe <70539716+johnsonhe@users.noreply.github.com>
2026-01-28 07:43:11 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
